1ca59404f7d83837d1b01f87b43c2b1709079d6b
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-04-24 12:29:52 -0400

    set up mongoose schema for author & articles

---
+++ api/models/article.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    tags: [{
+        type: String
+    }]
+});
+
+ArticleSchema.virtual('date').get(function() {
+    return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_MED);
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
---
+++ api/models/author.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
