51066c0a1440151b16d0c7a565bb56707d4b7ce2
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-04-24 17:11:09 -0400

    get login working without passportjs

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 /* GET admin log-in */
 router.get('/', function(req, res, next) {
     res.render('login');
 });
 
 // POST admin log-in
-router.post('/', function(req, res, next) {
-    res.send(req.body);
-})
+router.post('/', async function(req, res, next) {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+        res.send('no user found');
+    } else if (user && user.password !== req.body.password) {
+        res.send('incorrect password');
+    } else {
+        res.send('logged in');
+    }
+});
 
 module.exports = router;
