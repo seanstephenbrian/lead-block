92afc0612c5dbf6dbcdfdbaa1f31f2e0faf4124c
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-04-24 20:11:15 -0400

    build out login & logout functionality

--- api/models/user.js
+++ api/models/user.js
@@ -10,6 +10,11 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    admin: {
+        type: Boolean,
+        require: true,
+        default: false
     }
 });
 
--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const User = require('../models/user');
 
@@ -9,15 +10,45 @@ router.get('/', function(req, res, next) {
 });
 
 // POST admin log-in
-router.post('/', async function(req, res, next) {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) {
-        res.send('no user found');
-    } else if (user && user.password !== req.body.password) {
-        res.send('incorrect password');
-    } else {
-        res.send('logged in');
-    }
+router.post('/',
+    passport.authenticate('local', {
+        successRedirect: '/admin/dashboard',
+        failureRedirect: '/admin'
+    })
+);
+
+// GET admin dashboard:
+router.get('/dashboard', function(req, res, next) {
+    res.render('dashboard', { user: req.user });
+});
+
+// GET log out:
+router.get('/log-out', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/admin');
+    });
+});
+
+// GET new user form:
+router.get('/new-user', function(req, res, next) {
+    res.render('new-user');
+});
+
+// POST new user:
+router.post('/new-user', async (req, res, next) => {
+    try {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+        const result = await user.save();
+        res.redirect('/admin');
+    } catch(err) {
+        return next(err);
+    };
 });
 
 module.exports = router;
