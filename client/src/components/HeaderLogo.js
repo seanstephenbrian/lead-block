a6b9ccc40b3c0a2a5654c5b0353dcadc2cfccc6d
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-25 17:35:41 -0400

    set up flicker animation

--- client/src/components/HeaderLogo.js
+++ client/src/components/HeaderLogo.js
@@ -1,10 +1,16 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { ThemeContext } from '../App';
+
+import '../style/logo-shadow.scss';
+
 export default function HeaderLogo() {
+
+    const theme = useContext(ThemeContext);
+
     return (
-        <Link className='logo' to='./'>
+        <Link className={`logo ${theme}`} to='./'>
             <div className='lead'>LEAD</div>
             <div className='block'>BLOCK</div>
             <div className='sports'>SPORTS</div>
