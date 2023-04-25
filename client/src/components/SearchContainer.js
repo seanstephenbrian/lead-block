f1cbb0d786821f9d54acc602f7b0519d0a767552
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-25 18:57:07 -0400

    continue styling search bar

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,4 +1,10 @@
+import React, { useState } from 'react';
+
 export default function SearchContainer() {
+
+    // store search bar expansion status in state:
+    const [searchExpanded, setSearchExpanded] = useState(false);
+    
     return (
         <div className="search-container">
         </div>
