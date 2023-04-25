940bcd6e3d80d15b7ceeee346c215d95c011d115
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-25 17:03:42 -0400

    start building out theme styles
    header menu icon for selected theme is marked with a circle border

--- client/src/components/HeaderMenu.js
+++ client/src/components/HeaderMenu.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ThemeContext } from '../App';
 
 import DarkIcon from '../img/svg/dark.svg';
 import HamburgerIcon from '../img/svg/hamburger.svg';
@@ -9,8 +11,10 @@ export default function HeaderMenu(props) {
 
     const { handleDarkClick, handleLightClick } = props;
 
+    const theme = useContext(ThemeContext);
+
     return (
-        <div className='menu'>
+        <div className={`menu ${theme}`}>
             <img className='menu-icon dark-icon hover-grow' onClick={handleDarkClick} src={DarkIcon} />
             <img className='menu-icon light-icon hover-grow' onClick={handleLightClick} src={LightIcon} />
             <img className='menu-icon search-icon hover-grow' src={SearchIcon} />
