82773c41315b43c29d35da4c7bd5b23d7c5491b5
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-04-26 14:01:30 -0400

    style footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import '../style/scss/footer.scss';
+
+import TwitterIcon from '../img/svg/twitter.svg';
+
 export default function Footer() {
     return (
-        <footer>
-            footer
+        <footer className='footer'>
+            <a href='https://twitter.com/lbkbear' className='twitter hover-grow' target='_blank' rel='noopener noreferrer'>
+                <img src={TwitterIcon} alt='Lead Block Sports Twitter' />
+            </a>
+            <div className='copyright'>
+                Copyright © Lead Block Sports {new Date().getFullYear()}
+            </div>
+            <div className='ssbbd'>
+                <div>site design by</div>
+                <div className='ssbbd-link'>
+                    <a href='https://ssbbd.dev/' target='_blank' rel='noopener noreferrer'>ssbbd</a>
+                </div>
+            </div>
         </footer>
     )
 }
