1d0edef3c321fb90c09cf9a5e31abeeec92a80f9
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-04-26 12:27:56 -0400

    style article tag section

--- client/src/components/ArticleTags.js
+++ client/src/components/ArticleTags.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function ArticleTags() {
+export default function ArticleTags(props) {
+
+    const { tags } = props;
+
+    const articleTags = tags.map((tag) => {
+        return (
+            <Link
+                className='tag hover-grow'
+                key={uuidv4()}
+                to={`../search/` + tag}
+            >
+                {tag}
+            </Link>
+        );
+    });
+
     return (
-        <></>
-    )
+        <div className='article-tags'>
+            {articleTags}
+        </div>
+    );
 }
