f5e7f61173594c21f021b672e5c37207e91799b4
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-25 23:52:47 -0400

    finish styling header

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function SearchBar() {
     return (
-        <input className='search-bar' type='text'/>
+        <>
+            <input className='search-bar hover-grow grow-less orange-hover-glow' type='text' />
+            <span className='submit-search'>Search</span>
+        </>
     )
 }
