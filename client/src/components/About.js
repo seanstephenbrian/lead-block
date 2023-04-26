5f7340404c9d21d8cd912f273debdafbfd8e2cc0
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-26 19:55:39 -0400

    build out about and contact pages

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../style/scss/info-page.scss';
 
 export default function About() {
     return (
-        <section className='about-page'>
-            about
+        <section className='about-page info-page'>
+            <h1>About Lead Block Sports</h1>
+            <p>Lead Block Sports is a platform for quality football and basketball analysis and other exciting updates from the wider world of sports.</p>
+            <p>The blog is maintained by Kevin and based out of northern Illinois.</p>
+            <p>Feel free to <Link class='inline-link' to='../contact'>reach out</Link> if you're interested in collaborating or contributing your content to the site! We're always looking to make new friends.</p>
+            <p>You can also find Lead Block <a class='inline-link' href='https://twitter.com/lbkbear' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
         </section>
     )
 }
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import '../style/scss/info-page.scss';
+
 export default function Contact() {
     return (
-        <section className='contact-page'>
-            contact
+        <section className='contact-page info-page'>
+            <h1>Contact</h1>
+            <p>You can reach Lead Block by email at <a href='mailto:leadblocksports@gmail.com' class='inline-link'>leadblocksports@gmail.com</a>.</p>
+            <p>We're also <a class='inline-link' href='https://twitter.com/lbkbear' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
         </section>
     )
 }
