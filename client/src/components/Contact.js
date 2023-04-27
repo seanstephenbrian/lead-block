b2dd98a088cb498041e2bae05941b024062036c8
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-27 13:02:44 -0400

    update text styles for contact/about pages

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     return (
         <section className='contact-page info-page'>
             <h1>Contact</h1>
-            <p>You can reach Lead Block by email at <a href='mailto:leadblocksports@gmail.com' className='inline-link'>leadblocksports@gmail.com</a>.</p>
+            <p>You can reach us by email at <a href='mailto:leadblocksports@gmail.com' className='inline-link'>leadblocksports@gmail.com</a>.</p>
             <p>We're also <a className='inline-link' href='https://twitter.com/lbkbear' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
         </section>
     )
