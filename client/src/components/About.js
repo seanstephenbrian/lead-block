5f3e3de0b6a904e999cfffa3139d6ed2846c595f
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-26 20:07:52 -0400

    correct typos

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,8 +9,8 @@ export default function About() {
             <h1>About Lead Block Sports</h1>
             <p>Lead Block Sports is a platform for quality football and basketball analysis and other exciting updates from the wider world of sports.</p>
             <p>The blog is maintained by Kevin and based out of northern Illinois.</p>
-            <p>Feel free to <Link class='inline-link' to='../contact'>reach out</Link> if you're interested in collaborating or contributing your content to the site! We're always looking to make new friends.</p>
-            <p>You can also find Lead Block <a class='inline-link' href='https://twitter.com/lbkbear' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
+            <p>Feel free to <Link className='inline-link' to='../contact'>reach out</Link> if you're interested in collaborating or contributing your content to the site! We're always looking to make new friends.</p>
+            <p>You can also find Lead Block <a className='inline-link' href='https://twitter.com/lbkbear' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
         </section>
     )
 }
