de23e33c5661fd145eadf4ec19a898aba22b8293
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-26 20:07:52 -0400

    correct typos

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -6,8 +6,8 @@ export default function Contact() {
     return (
         <section className='contact-page info-page'>
             <h1>Contact</h1>
-            <p>You can reach Lead Block by email at <a href='mailto:leadblocksports@gmail.com' class='inline-link'>leadblocksports@gmail.com</a>.</p>
-            <p>We're also <a class='inline-link' href='https://twitter.com/lbkbear' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
+            <p>You can reach Lead Block by email at <a href='mailto:leadblocksports@gmail.com' className='inline-link'>leadblocksports@gmail.com</a>.</p>
+            <p>We're also <a className='inline-link' href='https://twitter.com/lbkbear' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
         </section>
     )
 }
