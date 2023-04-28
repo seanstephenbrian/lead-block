e84b59d11bd4930da04c7050a0bb70acc39e9525
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-27 23:52:28 -0400

    update twitter links

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
         <section className='contact-page info-page'>
             <h1>Contact</h1>
             <p>You can reach us by email at <a href='mailto:leadblocksports@gmail.com' className='inline-link'>leadblocksports@gmail.com</a>.</p>
-            <p>We're also <a className='inline-link' href='https://twitter.com/lbkbear' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
+            <p>We're also <a className='inline-link' href='https://twitter.com/leadblocksports' target='_blank' rel='noopener noreferrer'>on Twitter!</a></p>
         </section>
     )
 }
