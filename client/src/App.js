9737d52057a12204bc9db650a77f696ddab3d49e
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-04-28 11:41:57 -0400

    store theme preference in localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 import Site from './components/Site';
 
@@ -10,10 +10,22 @@ function App() {
     // store theme in site state:
     const [theme, setTheme] = useState('light');
 
+    // check for saved theme preference in localStorage on app launch:
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme');
+        if (storedTheme) {
+            setTheme(storedTheme);
+        }
+    }, []);
+
     return (
         <ThemeContext.Provider value={theme}>
             <div className={`site-wrapper ${theme}`}>
-                <Site updateTheme={(newTheme) => setTheme(newTheme)} />
+                <Site updateTheme={(newTheme) => {
+                    setTheme(newTheme);
+                    localStorage.setItem('theme', newTheme);
+                }} 
+                />
             </div>
         </ThemeContext.Provider>
     );
